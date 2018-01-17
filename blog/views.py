4adc65a7cb63943b8df5bc3f1fe2ade9b158a49e
Your Name <you@example.com>
2018-01-17 17:38:23 +0530

    we want our post want the posts to be displayed like the static posts we created earlier in the Introduction to HTML chapter. You can mix HTML and template tags

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.utils import timezone
+
 #To including code -we need to include model we have written in the models.py file - import
 from .models import Post
 
