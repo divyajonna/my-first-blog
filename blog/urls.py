1aa56368da4f4b6b911d16065d561dcd2342032d
Your Name <you@example.com>
2018-01-19 10:45:23 +0530

    URL in the  file to point Django to a view named post_detail, that will show an entire blog post

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     url(r'^$', views.post_list, name='post_list'),
+
+    #URL in the  file to point Django to a view named post_detail, that will show an entire blog post
+
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'), 
 ]
