3043cfa04992a8f83a101bf4c73251a2e3fadb99
Your Name <you@example.com>
2018-01-19 14:50:07 +0530

    created form in blog

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import post
+
+#PostForm is the name of our Form
+class PostForm(forms.ModelForm):
+#we need to tell django -that this model is ModelForm
+
+
+    class Meta:
+        #here we tell Django which model should be used to create this form (model = Post).
+        model = Post
+        fields = ('title','text')
