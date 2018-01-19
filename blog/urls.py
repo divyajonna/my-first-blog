9f0d757782cf22c03bf44ef57ded299e9fa7e1c9
Your Name <you@example.com>
2018-01-19 15:06:54 +0530

    added url for forms

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns=[
 
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 
+    #URL in the  file to point Django to a view named post_new, that will show an entire blog post
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
