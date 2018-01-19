af7b0834eeb84c54d0886f69cb381dd0a145e4cf
Your Name <you@example.com>
2018-01-19 15:43:00 +0530

    created new post

--- blog/forms.py
+++ blog/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 
-from .models import post
+from .models import Post
 
 #PostForm is the name of our Form
 class PostForm(forms.ModelForm):
 #we need to tell django -that this model is ModelForm
 
-
     class Meta:
         #here we tell Django which model should be used to create this form (model = Post).
         model = Post
-        fields = ('title','text') #fields to end up in this form
+        fields = ('title', 'text',)
+        #fields to end up in this form
--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,5 @@ urlpatterns=[
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 
     #URL in the  file to point Django to a view named post_new, that will show an entire blog post
-    url(r'^post/new/$', views.post_new, name='post_new'),
+     url(r'^post/new/$', views.post_new, name='post_new'),
 ]
